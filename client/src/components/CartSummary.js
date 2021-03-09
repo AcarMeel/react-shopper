1042400b4960276a99b2d2125726f256449385de
acarmeel <acarmeel@MacBook-Pro-de-acarmeel.local>
2021-03-09 17:18:39 -0600

    Cart Modal

--- client/src/components/CartSummary.js
+++ client/src/components/CartSummary.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useShoppingCart } from "use-shopping-cart";
+import CartModal from "./CartModal";
 import { CartIcon } from "./Icons";
 
 export default function CartSummary() {
   const { cartCount, formattedTotalPrice } = useShoppingCart();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => setIsOpen(!isOpen);
+
   return (
     <>
-      <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <nav onClick={toggleModal} className="md:ml-auto flex flex-wrap items-center text-base justify-center">
         <span className="mr-5 hover:text-white flex items-center">
           <CartIcon />
           <span className="ml-3">{formattedTotalPrice} ({cartCount})</span>
         </span>
       </nav>
+      <CartModal isOpen={isOpen} toggleModal={toggleModal} />
     </>
   );
 }
